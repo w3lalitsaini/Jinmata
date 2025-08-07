36973f24c5ff0c188af70072bea5bcb11cbf91bb
Lalit saini <sainilalit2751@gmail.com>
2025-08-07 20:10:23 +0530

    Footer

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
